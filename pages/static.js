0326fe1bf3848be2b62c5b715a5c9e45cb4fbc19
Tom Sisman <tomsisman@gmail.com>
2021-06-22 19:27:27 +0100

    styles + add more images

--- pages/static.js
+++ pages/static.js
@@ -4,15 +4,16 @@ function GridItem({props}) {
   const {urls, alt_description, description} = props;
   return (
     <div className="border-2 p-4 relative">
-      <Image src={urls.small} alt={alt_description} height={300} width={400} layout="responsive"/>
+      <Image src={urls.small} alt={alt_description} height={300} width={400} layout="responsive" placeholder="blur" blurDataURL={urls.thumb} />
 
-      <p className="mt-4">{description}</p>
+      <p className="mt-4 break-all">{description}</p>
     </div>
   )
 }
 
 export default function StaticPage({data}) {
   const {results} = data;
+  console.log(results)
   return (
     <div className="mx-4">
       <h1 className="text-4xl text-center my-16">Static Site Generated.</h1>
@@ -20,7 +21,7 @@ export default function StaticPage({data}) {
 
       <p className="text-lg text-center mb-16 max-w-5xl mx-auto">Morbi at quam quam. Aliquam vel elit at lacus feugiat tempor ut ac elit. Sed at fringilla massa. Nullam vel justo lectus. Nullam faucibus ultrices nisl, cursus interdum purus pellentesque ac. Praesent quis lacus quis mauris venenatis pellentesque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin vitae lacus nec tortor hendrerit feugiat. Aenean odio neque, posuere vitae facilisis eu, commodo at purus. Morbi rhoncus lorem sed nibh aliquet, non commodo justo ornare. Aenean vitae mollis massa.</p>
 
-      <div className="grid gap-4 grid-cols-3">
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         {
           results.map((item) => <GridItem key={item.id} props={item} />)
         }
@@ -34,7 +35,7 @@ export default function StaticPage({data}) {
 }
 
 export async function getStaticProps(context) {
-  const res = await fetch(`https://api.unsplash.com/search/photos?page=1&query=burgers&client_id=${process.env.UNSPLASHKEY}=1&per_page=9`)
+  const res = await fetch(`https://api.unsplash.com/search/photos?page=1&query=burgers&client_id=${process.env.UNSPLASHKEY}=1&per_page=36`)
   const data = await res.json()
 
   if (!data) {
